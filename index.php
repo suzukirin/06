0b13fcd86fad2e8c3cda540eb54b4f2c5f2fd85e
suzukirin <qilin.liberty124@gmail.com>
2020-10-20 21:45:34 +0900

    first commit

--- index.php
+++ index.php
@@ -2,8 +2,54 @@
 
 // 以下にCarクラスを定義して、当プログラムを実行した時にエラーがでないようにして下さい。
 
+class Car
+{
+    private $name;
+    private $number;
+    private $color;
+    
 
+    public function __construct($name,$number,$color)
+    {
+        $this ->name = $name;
+        $this ->number = $number;
+        $this ->color = $color;
+        
+    }
+    
+    public function getName()
+    {
+        return $this ->name;
+    }
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+    
+    public function getNumber()
+    {
+        return $this ->number;
+    }
+    public function setNumber($number)
+    {
+        $this->number = $number;
+    }
 
+    public function getColor()
+    {
+        return $this ->color;
+    }
+    public function setColor($color)
+    {
+        $this->color = $color;
+    }
+
+    public function infomation()
+    {
+        echo '車の車種:' .$this->name .'、車体番号:'. $this->number . '、カラー:' . $this->color .'です。';
+    }
+
+}
 
 
 // 以下を実行した時にエラーがでないようにして下さい。
